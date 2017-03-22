39682da187adb7d5128341095408ea76f8556c05
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-03-22 17:10:37 +0100

    Add test files of the two next components

---
+++ tests/IdentifyCurrentStateTest.java
@@ -0,0 +1,14 @@
+package hdss.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hdss.WatershedManagerInterface;
+import hdss.exceptions.HydricDSSException;
+
+public class IdentifyCurrentStateTest {
+
+	private WatershedManagerInterface manager;
+
+}
---
+++ tests/RegisterIrrigationDemandTest.java
@@ -0,0 +1,14 @@
+package hdss.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hdss.WatershedManagerInterface;
+import hdss.exceptions.HydricDSSException;
+
+public class RegisterIrrigationDemandTest {
+
+	private WatershedManagerInterface manager;
+
+}
