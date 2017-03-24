ade4ddea31cbec59cb9d0320d7ab52a0da07e52e
GuillermoEscobero <100346060@alumnos.uc3m.es>
2017-03-24 09:58:11 +0100

    Add WatershedManager.java

---
+++ src/hdss/WatershedManager.java
@@ -0,0 +1,31 @@
+package hdss;
+
+import hdss.exceptions.HydricDSSException;
+import hdss.io.IrrigationDemandPublicData;
+import hdss.io.ScenarioPublicData;
+import hdss.io.WatershedsListPublicData;
+
+public class WatershedManager implements WatershedManagerInterface {
+
+	@Override
+	public WatershedsListPublicData RegisterHydricResources(String InputFile)
+			throws HydricDSSException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ScenarioPublicData IdentifyCurrentState(String InputFile,
+			String Scope) throws HydricDSSException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IrrigationDemandPublicData RegisterIrrigationDemand(String InputFile)
+			throws HydricDSSException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
